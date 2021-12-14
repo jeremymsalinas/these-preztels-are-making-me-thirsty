86ecfae752d0c2c93d59e77a4f8a7cfee87e3231
jeremymsalinas <45369791+jeremymsalinas@users.noreply.github.com>
2021-12-13 18:19:18 -0800

    Python API

---
+++ Python/freshServiceToSherpaDeskAPISample.py
@@ -0,0 +1,124 @@
+
+import pprint, requests, json
+from datetime import datetime, timedelta
+
+# this script imports time details from fresh service API and creates a ticket and adds time using the sherpa desk API
+
+# pretty print instance for testing output
+pp = pprint.PrettyPrinter(indent=2, width=80, compact=False)
+
+# fresh service API key
+freshAPI = "FRESH_API_KEY"
+domain = "dnae"
+# freshservice value
+freshValue = "x"
+
+# Create timedelta for today minus 12 hours
+weekOf = datetime.now() - timedelta(hours=12)
+message = ''
+
+# Check if today is a weekday, if it's not exit the script
+today = datetime.today().strftime('%A')
+
+if today == 'Saturday' or today == 'Sunday':
+  exit()
+
+# Return the tickets that are new or opend & assigned to you
+# If you want to fetch all tickets remove the filter query param
+r = requests.get("https://"+ domain +".freshservice.com/api/v2/time_entries?executed_after="+ weekOf.isoformat()[:-7] +"Z", auth = (freshAPI, freshValue))
+
+# if the request is successful proceed
+if r.status_code == 200:
+  print("Request processed successfully, the response is given below")
+  response = r.json()
+  c = 0
+  totalHours = 0
+  totalMinutes = 0
+  pp.pprint(response)
+  # loop through tickets and add time in hours and minutes for all tickets assosciated with your agent id
+  for i in response:
+    if response[c]['agent_id'] == YOUR_AGENT_ID:
+      totalHours += int(response[c]['time_spent'][:2])
+      totalMinutes += int(response[c]['time_spent'][3:])
+      c += 1
+    else:
+      c += 1
+  
+  # Create a new timedelta from time spent on tickets
+  freshTime = timedelta(hours=totalHours,minutes=totalMinutes)
+
+  # convert to seconds
+  totalSeconds = freshTime.total_seconds()
+
+  # convert total seconds to hours, minutes, and seconds
+  h = totalSeconds//3600
+  m = (totalSeconds%3600) // 60
+  # sec =(totalSeconds%3600)%60
+
+  # format total time spent in hours:minutes
+  timeSpent = "%d:%d" %(h,m)
+
+  # Format message that will be sent in report
+  message = f"Total time spent on tickets for DNAe {datetime.now().strftime('%m/%d/%Y')} was {timeSpent} hours."
+  # Check message
+  print(message)
+
+# If the request fails display the error messages   
+else:
+  print("Failed to read tickets, errors are displayed below,")
+  response = json.loads(r.content)
+  print(response["errors"])
+
+  print("x-request-id : " + r.headers['x-request-id'])
+  print("Status Code : " + str(r.status_code))
+
+# Send info to Sherpadesk and open ticket
+sherpaKey = "YOUR_SHERPA_KEY"
+sherpaValue = "YOUR_SHERPA_VALUE"
+
+tech_id = 'YOUR_TECH_ID'
+user_id = 'USER_ID_FOR_TICKET'
+
+url = "https://"+ sherpaKey +":"+ sherpaValue +"@api.sherpadesk.com/tickets?format=json"
+
+payload={'account_id': '0',
+'class_id': '0',
+'initial_post': 'Hello,\n\n\n'+ message +'\n\nThanks,\nJeremy',
+'location_id': '0',
+'status': 'open',
+'subject': 'Time Summary PartsUnlimited',
+'tech_id': tech_id,
+'user_id': user_id}
+files=[
+
+]
+headers = {}
+
+# Retrieve response from ticket creation and add time to new ticket
+r = requests.request("POST", url, headers=headers, data=payload, files=files)
+response = r.json()
+
+# store ticket id and ticket key from response to use in time post
+ticket_id = response['number']
+ticket_key = response['key']
+
+url = "https://"+ sherpaKey +":"+ sherpaValue +"@api.sherpadesk.com/time?format=json"
+
+payload={'account_id': '-1',
+'hours': str(h+round(m/60,2)),
+'no_invoice': 'true',
+'is_project_log': 'true',
+'note_text': 'Adding time for DNAe',
+'project_id': '0',
+'task_type_id': 41712,
+'tech_id': tech_id,
+'ticket_key': ticket_key}
+files=[
+
+]
+headers = {}
+
+response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+# prettyprint the response
+pp.print(response.text)
